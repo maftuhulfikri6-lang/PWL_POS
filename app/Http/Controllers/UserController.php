8751c4530ac6ce28b0a4bd82e7cb977557eabe97
maftuhulfikri6-lang <maftuhulfikri6@gmail.com>
2026-04-28 14:32:38 +0700

    Praktikum: Menggunakan method save() untuk menyimpan objek firstOrNew ke database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-    // Menghitung jumlah user dengan level_id = 2
-    $user = UserModel::where('level_id', 2)->count();
+        // 1. Membuat instance objek baru di memori jika 'manager33' tidak ditemukan
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
 
-    // Mengirim hasil hitungan (angka) ke view
-    return view('user', ['data' => $user]);
-}
+        // 2. Menyimpan objek tersebut dari memori ke database secara permanen
+        $user->save();
+
+        return view('user', ['data' => $user]);
     }
+}
