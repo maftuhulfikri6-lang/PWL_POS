abc93f560a5a03efcbd04258288f58c652be6c6c
maftuhulfikri6-lang <maftuhulfikri6@gmail.com>
2026-04-28 13:39:10 +0700

    Selesai Praktikum: Implementasi findOr dengan ID 20 dan penanganan error 404

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,23 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\UserModel;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request; // Tambahkan ini agar standar controller lengkap
 
 class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_dua',
-            'nama' => 'Manager 2',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // Mencari user dengan ID 20. 
+        // Jika data tidak ditemukan, maka fungsi abort(404) akan dijalankan.
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
 
-        $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,25 +1,21 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Data User</title>
-    </head>
-    <body>
-        <h1>Data User</h1>
-        <table border="1" cellpadding="2" cellspacing="0">
+<body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <thead>
             <tr>
                 <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
             </tr>
-            @foreach ($data as $d)
+        </thead>
+        <tbody>
             <tr>
-                <td>{{ $d->user_id }}</td>
-                <td>{{ $d->username }}</td>
-                <td>{{ $d->nama }}</td>
-                <td>{{ $d->level_id }}</td>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
             </tr>
-            @endforeach
-        </table>
-    </body>
-</html>
+        </tbody>
+    </table>
+</body>
